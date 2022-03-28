797874719434b6f883c9ec32ee35f3670991f740
HaoliangZhao <haoliang.zhao@ximalaya.com>
2022-03-28 11:28:30 +0800

    first commit

---
+++ config/webpack.common.js
@@ -0,0 +1,60 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+    entry: {
+        app: './src/index.tsx',
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            title: 'dev',
+            template: './public/index.html'
+        }),
+    ],
+    output: {
+        filename: '[name].bundle.js',
+        path: path.resolve(__dirname, '../dist'),
+        clean: true,
+    },
+    resolve: {
+        extensions: ['.js', '.jsx', ".tsx", ".ts"]
+    },
+    module: {
+        rules: [
+            {
+                test: /\.(png|svg|jpg|jpeg|gif)$/i,
+                type: 'asset/resource',
+            },
+            {
+                test: /\.(woff|woff2|eot|ttf|otf)$/i,
+                type: 'asset/resource',
+            },
+            {
+                test: /\.tsx?$/,
+                use: 'ts-loader',
+                exclude: /node_modules/,
+            },
+            {
+                test: /(\.jsx|\.js)$/,
+                use: {
+                    loader: 'babel-loader',
+                    options: {
+                        presets: ['@babel/preset-env']
+                    }
+                },
+                exclude: /node_modules/
+            },
+            {
+                test: /\.s[ac]ss$/i,
+                use: [
+                    // 将 JS 字符串生成为 style 节点
+                    'style-loader',
+                    // 将 CSS 转化成 CommonJS 模块
+                    'css-loader',
+                    // 将 Sass 编译成 CSS
+                    'sass-loader',
+                ],
+            },
+        ],
+    },
+};
---
+++ src/app.tsx
@@ -0,0 +1,7 @@
+import React, { FC } from 'react';
+const App: FC<any> = () => {
+    return (
+        <div>hello world</div>
+    )
+}
+export default App
