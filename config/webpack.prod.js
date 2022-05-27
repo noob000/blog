cdffd3407d6eb5c32397043cca481225aa032063
HaoliangZhao <haoliang.zhao@ximalaya.com>
2022-05-27 10:46:40 +0800

    feat(renderer): 优化打包体积

--- config/webpack.prod.js
+++ config/webpack.prod.js
@@ -1,7 +1,55 @@
 const { merge } = require('webpack-merge');
- const common = require('./webpack.common.js');
-
- module.exports = merge(common, {
-   mode: 'production',
-   devtool: 'source-map',
- });
+const common = require('./webpack.common.js');
+const BundleAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPlugin;
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+module.exports = merge(common, {
+  mode: 'production',
+  devtool: 'source-map',
+  output: {
+    publicPath: "./"
+  },
+  plugins: [
+    new BundleAnalyzerPlugin(),
+    new MiniCssExtractPlugin({
+      attributes: {
+        id: "target",
+        "data-target": "example",
+      },
+    }),
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: [MiniCssExtractPlugin.loader, "css-loader"],
+      },
+    ],
+  },
+  optimization: {
+    splitChunks: {
+      chunks: 'async',
+      minSize: 20000,
+      minRemainingSize: 0,
+      minChunks: 1,
+      maxAsyncRequests: 30,
+      maxInitialRequests: 30,
+      enforceSizeThreshold: 50000,
+      cacheGroups: {
+        defaultVendors: {
+          test: /[\\/]node_modules[\\/]/,
+          priority: -10,
+          reuseExistingChunk: true,
+        },
+        default: {
+          minChunks: 2,
+          priority: -20,
+          reuseExistingChunk: true,
+        },
+      },
+    },
+  },
+  externals:{
+    react:"react",
+    "react-dom":"react-dom"
+  }
+});
